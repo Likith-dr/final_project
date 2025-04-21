9b196cefa9d64025ecd3bb689941c54acd01ba07
Likith-dr <rajumpel@gmail.com>
2025-04-21 15:39:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -215,5 +215,9 @@ def api_report_data():
 
 # ++++++++++++++++++++++++++++++++++++++ END OF REPORT PAGE +++++++++++++++++++++++++++++
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
